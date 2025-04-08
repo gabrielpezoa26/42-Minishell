396c21807ec263e111fe1255d7d95d6858c15fc7
Gabriel Cesar Nunes Pezoa <gcesar-n@c3r13p3.42sp.org.br>
2025-04-08 17:36:07 -0300

    -

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:40:00 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/04/07 14:35:36 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/04/08 17:24:58 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 
 # include "libft/libft.h"
 # include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 /*PARSER*/
-
-int validate_input(char **argv);
+typedef struct s_data
+{
+	char	**envp;
+}	t_data;
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,17 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:37:42 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/04/07 11:46:51 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/04/08 17:26:12 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main(int argc, char **argv)
+// void	initialize(void)
+// {
+// 	while (1)
+// 	{
+// 		loop;
+// 	}
+// }
+
+int main(int argc, char **argv, char **envp)
 {
-	validate_input(argv);
+	t_data	data;
+
+	(void)argv;
+	(void)argc;
+	for (int i = 0; envp[i]; i++)
+		printf("%s\n", envp[i]);
+//	initialize();
 	return (0);
 }
