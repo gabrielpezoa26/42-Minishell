49716d2b7b2726412fe583cc704079f523caba58
Diego Teruya <dteruya@c1r2p4.42sp.org.br>
2025-05-13 17:05:59 -0300

    prints

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:22:33 by dteruya           #+#    #+#             */
-/*   Updated: 2025/05/13 12:12:14 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/05/13 16:39:35 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,9 @@ void	parse_input(t_data *data)
 	if (data->input == NULL)
 		exit_minishell(data, "DEBUG: finishhhh");
 	else if (ft_strcmp(data->input, "\0") == 0)
-	{
-		printf("DEBUG: eh nulo\n");
 		data->exec = false;
-	}
 	else if (verify_space(data->input))
-	{
-		printf("DEBUG: eh espaço\n");
 		data->exec = false;
-	}
 	if (!to_token(data))
 		data->exec = false;
 	add_history(data->input);
--- src/token/token.c
+++ src/token/token.c
@@ -6,7 +6,7 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 11:41:15 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/05/13 12:08:17 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/05/13 16:43:16 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ bool	to_token(t_data *data)
 {
 	if (!are_quotes_valid(data->input))
 	{
-		printf("Error!\n");
+		printf("%s\n", TOKEN);
 		return (false);
 	}
 	if (token_operators(data->input))
 	{
-		printf("Error!\n");
+		printf("%s\n", TOKEN);
 		return (false);
 	}
 	return (true);
