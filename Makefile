5f5149824af66f21a00c850fd8327336c2d3d0e0
Diego Teruya <dteruya@c3r1p5.42sp.org.br>
2025-05-16 16:15:22 -0300

    probably tokens are checked

--- Makefile
+++ Makefile
@@ -21,6 +21,7 @@ SRCS = $(SRC_DIR)/main.c \
 		$(TOKEN_DIR)/operators.c \
 		$(TOKEN_DIR)/quotes.c \
 		$(TOKEN_DIR)/token_nodes.c \
+		$(TOKEN_DIR)/token_nodes_utils.c \
 		$(TOKEN_DIR)/token_init.c \
 		$(TOKEN_DIR)/token_utils.c \
 		$(UTILS_DIR)/utils_parser.c \
--- src/token/token_nodes.c
+++ src/token/token_nodes.c
@@ -3,32 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   token_nodes.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 15:23:00 by dteruya           #+#    #+#             */
-/*   Updated: 2025/05/15 17:16:55 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:48:10 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static t_token	*init_node(char	*content, int operator)
+static t_token	*init_node(char	*content, int operator, bool is_BOOM)
 {
 	t_token	*new_node;
+	int		size;
 
 	new_node = (t_token *)ft_malloc(sizeof(t_token));
 	new_node->str = ft_strdup(content);
 	new_node->type = operator;
-	new_node->is_expandable = false;
+	new_node->is_expandable = is_BOOM;
+	new_node->array = NULL;
+	if (is_BOOM)
+	{
+		size = word_count(new_node->str);
+		new_node->array = ft_calloc(size, sizeof(int));
+		find_money(new_node->array, new_node->str, size);
+	}
 	new_node->next_token = NULL;
 	return (new_node);
 }
 
-void	append_node(t_token **tokens, char *content, int operator)
+void	append_node(t_token **tokens, char *content, int operator, bool is_BOOM)
 {
 	t_token	*node;
 
-	node = init_node(content, operator);
+	node = init_node(content, operator, is_BOOM);
 	if (*tokens == NULL)
 		*tokens = node;
 	else
