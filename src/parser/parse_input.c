84c6c4c73b29105dfd4281433170a26031b3cb0a
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-06-23 18:47:48 -0300

    testerrrr

--- src/parser/parse_input.c
+++ src/parser/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:22:33 by dteruya           #+#    #+#             */
-/*   Updated: 2025/06/23 16:06:56 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/06/23 18:47:12 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ bool	check_argc(int argc)
 	return (true);
 }
 
-void	debug_print_tokens(t_token *tokens)
-{
-	int	i;
+// void	debug_print_tokens(t_token *tokens)
+// {
+// 	int	i;
 
-	i = 0;
-	while (tokens)
-	{
-		ft_printf("DEBUG: Token %d: str='%s' | type=%d | BOOM=%d\n",
-			i, tokens->str, tokens->type, tokens->is_expandable);
-		tokens = tokens->next;
-		i++;
-	}
-}
+// 	i = 0;
+// 	while (tokens)
+// 	{
+// 		ft_printf("DEBUG: Token %d: str='%s' | type=%d | BOOM=%d\n",
+// 			i, tokens->str, tokens->type, tokens->is_expandable);
+// 		tokens = tokens->next;
+// 		i++;
+// 	}
+// }
 
 static bool	is_all_spaces(char *input)
 {
@@ -65,21 +65,12 @@ bool	parse_input(t_data *data, t_token **tokens, t_env **my_envp)
 	if (!data->input)
 		exit_minishell(data, 0);
 	else if (ft_strcmp(data->input, "\0") == 0)
-	{
-		printf("DEBUG: enter\n");
 		return (false);
-	}
 	else if (is_all_spaces(data->input))
-	{
-		printf("DEBUG: td espaço\n");
 		return (true);
-	}
 	if (!convert_token(data, tokens))
-	{
-		printf("DEBUG: convert_token deu ruim\n");
 		return (false);
-	}
-	debug_print_tokens(*tokens);
+	// debug_print_tokens(*tokens);
 	handle_assignments(*tokens, &data->locals);
 	return (true);
 }
